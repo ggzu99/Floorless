7e29b7cce22034580f7a4fbb09737a31f9c3487e
Tchy258 <tchy258@yahoo.cl>
2022-08-29 13:29:30 -0400

    Menu principal, assets de ui, dash
    Se creo escena de menu principal, se agregaron botones de play, creditos (no hace nada) y exit.
    Implementación incompleta del dash (falta arreglar bugs de animaciónes)

---
+++ Scripts/main_menu.gd
@@ -0,0 +1,29 @@
+extends MarginContainer
+
+onready var play = $VBoxContainer/Play
+onready var credits = $VBoxContainer/Credits
+onready var exit = $VBoxContainer/Exit
+
+
+func _ready():
+	play.connect("pressed", self, "_on_play_pressed")
+	credits.connect("pressed", self, "_on_credits_pressed")
+	exit.connect("pressed", self, "_on_exit_pressed")
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+func _on_play_pressed():
+	get_tree().change_scene("res://scenes/main.tscn")
+
+
+func _on_credits_pressed():
+	print("· o ·)")
+
+
+func _on_exit_pressed():
+	get_tree().quit()
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
--- Scripts/player_character.gd
+++ Scripts/player_character.gd
@@ -6,6 +6,7 @@ var SPEED = 200
 var JUMP_SPEED = 200
 var ACCELERATION = 2000
 var velocity = Vector2()
+var dash_time = 0
 
 onready var pivot = $Pivot
 onready var animation_player = $AnimationPlayer
@@ -17,52 +18,74 @@ func _ready():
 	playback.travel("fall")
 	
 func _physics_process(delta):
+	var current_animation = playback.get_current_node()
 	velocity = move_and_slide(velocity,Vector2.UP)
 	var horizontal_input = Input.get_axis("move_left","move_right")
 	var vertical_input = Input.get_axis("move_up","move_down")
-	velocity.x = move_toward(velocity.x,horizontal_input * SPEED,ACCELERATION)
-	if velocity.y < 100:
+	if dash_time==0 or current_animation!="dash":
+		velocity.x = move_toward(velocity.x,horizontal_input * SPEED,ACCELERATION)
+	else:
+		if velocity.x>0:
+			velocity.x = move_toward(velocity.x,SPEED*3,ACCELERATION*2)
+		else:
+			velocity.x = move_toward(velocity.x,-SPEED*3,ACCELERATION*2)
+	if velocity.y < 100 and dash_time == 0:
 		velocity.y += GRAVITY
-	if is_on_wall() and Input.is_action_just_pressed("jump"):
+	if Input.is_action_just_pressed("dash") and dash_time==0:
+		dash_time = 9*delta
+		velocity.x = horizontal_input * SPEED*3
+	if is_on_wall() and Input.is_action_just_pressed("jump") and dash_time==0:
 		velocity.y = -JUMP_SPEED
 	#Animations	
 	if is_on_wall():
-		if Input.is_action_pressed("move_right") and not Input.is_action_pressed("move_left"):
-			pivot.scale.y=-1
-			pivot.rotation_degrees = 90
-			if velocity.y>0:
+		if dash_time==0:
+			if Input.is_action_pressed("move_right") and not Input.is_action_pressed("move_left"):
+				pivot.scale.y=-1
+				pivot.rotation_degrees = 90
+				if velocity.y>0:
+					pivot.scale.x=1
+				else:
+					pivot.scale.x=-1
+				playback.travel("run")
+			if Input.is_action_pressed("move_left") and not Input.is_action_pressed("move_right"):
+				pivot.scale.y=-1
 				pivot.scale.x=1
+				pivot.rotation_degrees = -90
+				if velocity.y>0:
+					pivot.scale.x=-1
+				playback.travel("run")
 			else:
-				pivot.scale.x=-1
-			playback.travel("run")
-		if Input.is_action_pressed("move_left") and not Input.is_action_pressed("move_right"):
-			pivot.scale.y=-1
-			pivot.scale.x=1
-			pivot.rotation_degrees = -90
+				if not Input.is_action_pressed("move_left") and not Input.is_action_pressed("move_right"):
+					pivot.scale.y=1
+					pivot.rotation_degrees=0
+					playback.travel("fall")
+		if not (is_on_wall()):
+			pivot.rotation_degrees = 0
+			pivot.scale.y=1
+			if dash_time==0:
+				print("bien")
+				if Input.is_action_pressed("move_left") and not (Input.is_action_pressed("move_right")):
+					pivot.scale.x=-1
+				if Input.is_action_pressed("move_right") and not (Input.is_action_pressed("move_left")):
+					pivot.scale.x=1
 			if velocity.y>0:
-				pivot.scale.x=-1
-			playback.travel("run")
-		else:
-			if not Input.is_action_pressed("move_left") and not Input.is_action_pressed("move_right"):
-				pivot.scale.y=1
-				pivot.rotation_degrees=0
 				playback.travel("fall")
-	if not (is_on_wall()):
-		pivot.rotation_degrees = 0
-		pivot.scale.y=1
-		if Input.is_action_pressed("move_left") and not (Input.is_action_pressed("move_right")):
-			pivot.scale.x=-1
-		if Input.is_action_pressed("move_right") and not (Input.is_action_pressed("move_left")):
-			pivot.scale.x=1
-		if velocity.y>0:
-			playback.travel("fall")
-		else:
-			playback.travel("jump")
-	if Input.is_action_pressed("slash"):
+			else:
+				playback.travel("jump")
+	if Input.is_action_pressed("slash") and dash_time==0:
 		playback.travel("wield_sword")
 		playback.travel("slash1")
-	if playback.get_current_node()=="slash1":
+	if current_animation=="slash1":
 		playback.travel("sheathe_sword")
+	if dash_time>0:
+		playback.travel("dash")
+		dash_time-=1*delta
+		if dash_time<0:
+			dash_time=0
+			if is_on_wall():
+				playback.travel("land")
+			else:
+				playback.travel("fall")
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
