d4df83cd154e7c93cec57f8520d66d97c9053fc1
Tchy258 <tchy258@yahoo.cl>
2022-09-03 21:58:29 -0400

    Menu de pausa

---
+++ pause_menu.gd
@@ -0,0 +1,29 @@
+extends MarginContainer
+
+onready var resume = $VBoxContainer/Resume
+onready var main_menu = $VBoxContainer/MainMenu
+onready var exit = $VBoxContainer/Exit
+
+
+func _ready():
+	resume.connect("pressed", self, "_on_resume_pressed")
+	main_menu.connect("pressed", self, "_on_main_menu_pressed")
+	exit.connect("pressed", self, "_on_exit_pressed")
+	visible=false
+
+func _input(event):
+	if event.is_action_pressed("pause"):
+		visible = !visible
+		get_tree().paused = visible
+
+
+func _on_resume_pressed():
+	get_tree().paused = false
+	visible = false
+
+func _on_main_menu_pressed():
+	get_tree().change_scene("res://scenes/Ui/main_menu.tscn")
+	get_tree().paused = false
+
+func _on_exit_pressed():
+	get_tree().quit()
